d449d69708836436950464a0ab889c3ee253239b
theophilus doku <theodoku89@gmail.com>
2023-04-18 14:42:08 +0000

    Modified store.js, renamed countries folder

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import continentsSlice from "./redux/continents/continentsSlice";
-import countriesSlice from "./redux/continents/countires/countriesSlice";
+import countriesSlice from "./redux/continents/countries/countriesSlice";
 
 const store = configureStore({
   reducer: {
